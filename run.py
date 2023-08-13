abedf9b5fc2cffb9d6c651f8817ad999afc3b8f4
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2023-08-13 04:55:05 +0200

    bugfix: Fix clear_tmnl() in List All Books feature

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def welcome_message():
     """
     A welcome message that provides the main menu of the application.
     """
-    clear_tmnl
+    clear_tmnl()
     print("Welcome to the Children's Book Picker\n")
     print("Please select an option below.\n")
     print(colored(("(1) List all books"), "green"))
@@ -59,37 +59,31 @@ def welcome_message():
             break
         
     if welcome_message_ans == ("1"):
-         main_list ()
+         load_books ()
     #elif welcome_message_ans == ("2"):
-        #early_childhood ()
+        #Random Book Picker ()
     #elif welcome_message_ans == ("3"):
-        #middle_childhood ()
-    #elif welcome_message_ans == ("4"):
-        #late_childhood ()
-    #elif welcome_message_ans == ("5"):
-        #adolescence ()
-    #elif welcome_message_ans == ("6"):
-       #search_book ()
+        #Search ()
         
-def main_list (): #FIXME: Change to load_books
+def load_books():
     """
     List all books in the spreadsheet. 
     """
-    clear_tmnl()
+    clear_tmnl() #FIXME: Where to puth the clear terminal statement? 
     all_books = SHEET.worksheet('main_list') 
     headerSpreadsheet = all_books.row_values(1)
     numberOfBooks = len(all_books.col_values(1))-1
     numberOfColumns = len(all_books.row_values(1))
        
     all_rows = []
-    for ind in range(1,22): #FIXME: Found out if 22 is the correct number in the spread sheet. 
-        all_col = all_books.col_values(ind) #FIXME: ind column name or Python tech?
+    for ind in range(1,22):
+        all_col = all_books.col_values(ind) 
         all_rows.append(all_col[1: ])
     title = all_rows[0]
     author = all_rows[1]
    
-    # author =  all_books.col_values(2)     #HK: alternative way to get author, first take all column including header(row1)
-    # author = author[1:]                   #HK: remove row 1 (since it is already in python, we start counting at 0, so we omit 0 and start at 1)
+    # TODO:author =  all_books.col_values(2)     #HK: alternative way to get author, first take all column including header(row1)
+    # TODO: author = author[1:]                   #HK: remove row 1 (since it is already in python, we start counting at 0, so we omit 0 and start at 1)
   
     illustrator = all_rows[2]
     interest_level = all_rows[3]
@@ -98,12 +92,10 @@ def main_list (): #FIXME: Change to load_books
     synopsis = all_rows[6]
     
  
-    # for row in all_books:
-        # print(row[0])
+    # TODO: for row in all_books:
+        # TODO: print(row[0])
       
-    print('\n Complete Book List:\n')
-    #FIXME: Code snippet
-                
+    print('\n Complete Book List:\n')            
     print('\n Book Titles:\n')
     for ind in range(numberOfBooks):
         print(f"{title[ind]} - {author[ind]}")
